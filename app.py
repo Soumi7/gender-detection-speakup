cf97c0de84157df7811771df594cfd99a46975ce
Soumi Bardhan <soumibardhan10@gmail.com>
2021-05-13 20:04:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask, request, jsonify, render_template, url_for, make_response
+
+import numpy as np
+app = Flask(__name__)
+import numpy as np
+import keras
+from keras.preprocessing import image
+from keras.callbacks import ModelCheckpoint,EarlyStopping
+from keras.layers import Dense, Activation, Dropout, Flatten, Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Activation
+from keras.layers import Conv2D, AveragePooling2D
+from keras.models import Model, Sequential
+
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+#from keras.models import load_weights
+
+classes = 2
+
+#VGG-Face model
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+ 
+model.add(Convolution2D(4096, (7, 7), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(4096, (1, 1), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(2622, (1, 1)))
+model.add(Flatten())
+model.add(Activation('softmax'))
+
+model.load_weights('./vgg_face_weights.h5')
+
+for layer in model.layers[:-7]:
+    layer.trainable = False
+
+base_model_output = Sequential()
+base_model_output = Convolution2D(classes, (1, 1), name='predictions')(model.layers[-4].output)
+base_model_output = Flatten()(base_model_output)
+base_model_output = Activation('softmax')(base_model_output)
+
+gender_model = Model(inputs=model.input, outputs=base_model_output)
+
+sgd = keras.optimizers.SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+
+gender_model.compile(loss='categorical_crossentropy'
+                  , optimizer=keras.optimizers.Adam()
+                  #, optimizer = sgd
+                  , metrics=['accuracy']
+                 )
+
+checkpointer = ModelCheckpoint(
+    filepath='./classification_gender_model.hdf5'
+    , monitor = "val_loss"
+    , verbose=1
+    , save_best_only=True
+    , mode = 'auto'
+)
+
+scores = []
+
+gender_model.load_weights("./gender_model_weights.h5")
+
+
+
+def loadImage(filepath):
+    test_img = image.load_img(filepath, target_size=(224, 224))
+    test_img = image.img_to_array(test_img)
+    test_img = np.expand_dims(test_img, axis = 0)
+    test_img /= 255
+    return test_img
+
+@app.route('/')
+def home():
+    return "Gender Detection API working perfectly!"
+
+@app.route('/predict_api',methods=['POST','GET'])
+def predict():
+    #for HTML GUI rendering
+    file = request.files['file']
+    print(file.filename)
+    file.save(file.filename)
+    print(str("./"+str(file.filename)))
+    prediction = gender_model.predict(loadImage("./"+str(file.filename)))
+    print(np.argmax(prediction))
+
+    gender = "Man" if np.argmax(prediction) == 1 else "Woman"
+
+    print("gender: ", gender)
+
+    return jsonify(label = gender)
+
+if __name__ == '__main__':
+    #web: python myApp.py runserver 0.0.0.0:$PORT
+    app.run(host='0.0.0.0',port=5000)
+    #app.run(host='0.0.0.0',port=5000)
